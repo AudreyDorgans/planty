12650879d8cdd3f85567c98ed735e6cd0069333b
 AudreyDorgans <audrey@audreys-imac.home>
2024-01-17 16:40:58 +0100

    ajout fonctionnalité hook admin

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -37,3 +37,34 @@ function banniere_image_func($atts)
 
     return $output;
 }
+
+/* HOOK ADMIN */
+
+function ajouter_lien_admin_menu_nav($items, $args) {
+    // Verif si user connecté ou non
+    if (is_user_logged_in() && $args->theme_location === 'primary') {
+        // récup du lien du tableau de bord / esc_url() / sécurité intégrée à WordPress pour échapper l'URL
+        $admin_link = admin_url();
+        $admin_menu_item = '<li class="menu-item menu-item-admin"><a href="' . esc_url($admin_link) . '">Admin</a></li>';
+
+        // transformation de la chaîne de caractères $items en tableau
+        $items_array = preg_split('/<\/li>/', $items);
+
+        // trouve l'index du deuxième élément dans le menu, soit la clé 1
+        $second_menu_item_index = 1;
+
+        /* $items_array: Le tableau sur lequel l'opération doit être effectuée.
+        $second_menu_item_index: L'indice (position) à partir duquel les éléments seront insérés ou supprimés.
+        0: Le nombre d'éléments à supprimer à partir de l'indice spécifié. Dans ce cas, insertion des éléments sans en supprimer, donc 0.
+        $admin_menu_item: Les éléments à insérer dans le tableau.*/
+
+        array_splice($items_array, $second_menu_item_index, 0, $admin_menu_item);
+
+        // Convertit de nouveau le tableau en chaîne de caractères
+        $items = implode('</li>', $items_array);
+    }
+
+    return $items;
+}
+
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin_menu_nav', 10, 2);
